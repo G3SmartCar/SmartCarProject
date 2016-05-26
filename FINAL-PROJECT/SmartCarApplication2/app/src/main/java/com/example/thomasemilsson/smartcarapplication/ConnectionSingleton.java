18c36ac5782631f0983ec65bc475f20cc6ae79c3
AMANDA <GUSHOFAM@STUDENT.GU.SE>
2016-05-26 23:09:58 +0200

    Adding Documentation

--- FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/ConnectionSingleton.java
+++ FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/ConnectionSingleton.java
@@ -12,7 +12,10 @@ public class ConnectionSingleton {
     protected ConnectionSingleton(ConnectionHandler connectionHandler){
         this.connectionHandler = connectionHandler;
     }
-    /*Singleton pattern to make sure only one instance of this class will be created throughtout the product*/
+    /**
+     * Singleton pattern to make sure only one instance of this class will be created throughtout the product
+     * @return an instance of this class
+     */
     public static synchronized ConnectionSingleton getInstance(){
         if(null == active){
             active = new ConnectionSingleton(connectionHandler);
