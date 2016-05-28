3dd448b3985f0f0de0d0476ae0642a8cfd110cf4
AMANDA <GUSHOFAM@STUDENT.GU.SE>
2016-05-28 11:44:52 +0200

    removed SlideMenu.java

--- FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/ConnectionActivity.java
+++ FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/ConnectionActivity.java
@@ -80,10 +80,10 @@ public class ConnectionActivity extends Activity
         //setSupportActionBar(toolbar);
         
     
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+       // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+       // ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+       // drawer.setDrawerListener(toggle);
+       // toggle.syncState();
         
        // NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
        // navigationView.setNavigationItemSelectedListener(this);
