233b5d3cbc32977543ce9e4e2cb544ae835d216d
ThomasEmilsson <thomasemilsson10@gmail.com>
2016-06-01 19:53:54 +0200

    Update Properties.java

--- FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/Properties.java
+++ FINAL-PROJECT/SmartCarApplication2/app/src/main/java/com/example/thomasemilsson/smartcarapplication/Properties.java
@@ -5,7 +5,8 @@ package com.example.thomasemilsson.smartcarapplication;
  */
 
 /**
- * TODO: Add description of this class
+ * Used to check whether a bluetooth paired device was pressed, 
+ * or whether the connect button under the Wifi Set IP textfield was pressed
  */
 
 public class Properties {
@@ -15,12 +16,6 @@ public class Properties {
 
     protected Properties(){}
     
-    /**
-     * TODO: Add method description
-     *@return an instance of this class
-     */
-    
-
     public static synchronized Properties getInstance(){
         if(null == active){
             active = new Properties();
